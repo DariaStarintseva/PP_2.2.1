5b45f60fbd8425108ac4c3136d3f64d6a152c2eb
DariaStarintseva <yudina.daria@inbox.ru>
2023-08-26 10:47:33 +0300

    2.2.1

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,62 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+
+   private SessionFactory sessionFactory;
+
+   @Autowired
+   public UserDaoImp(SessionFactory sessionFactory) {
+      this.sessionFactory = sessionFactory;
+   }
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public void add(Car car) {
+      sessionFactory.getCurrentSession().save(car);
+   }
+
+   @Override
+   public List<Car> listCars() {
+      TypedQuery<Car> query=sessionFactory.getCurrentSession().createQuery("from Car");
+      return query.getResultList();
+   }
+
+   public User getUserByCarModelAndSeries(String model, int series) {
+
+      String hql = "SELECT u FROM User u WHERE u.car.model = :model AND u.car.series = :series";
+      Query<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+
+      List<User> users = query.getResultList();
+      if (users.isEmpty()) {
+         return null;
+      } else {
+         return users.get(0);
+      }
+
+   }
+
+}
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,19 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+
+    void add(Car car);
+
+    List<Car> listCars();
+
+    public User getUserByCarModelAndSeries(String model, int series);
+
+
+}
